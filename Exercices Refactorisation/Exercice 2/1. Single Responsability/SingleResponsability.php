4255e8a7c88dfacc747a2585942bfe9edc062c6d
Renaud Bresson <renaud.cci@gmail.com>
2023-12-12 13:28:00 +0100

    fin-exo2-1

--- Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
+++ Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
@@ -2,7 +2,18 @@
 
 namespace SRPViolation;
 
-class Modem
+interface ModemImplementationInterface
+{
+    public function dial($pno);
+
+    public function hangup();
+
+    public function send($c);
+
+    public function receive();
+}
+
+class ModemImplementation implements ModemImplementationInterface
 {
     public function dial($pno)
     {
@@ -23,4 +34,29 @@ class Modem
     {
         // Implementing receive() method.
     }
+}
+class DataChannel extends ModemImplementation
+{
+    public function send($c)
+    {
+        // Implementing send($c) method.
+    }
+
+    public function receive()
+    {
+        // Implementing receive() method.
+    }
+}
+class Connection extends ModemImplementation
+{
+    public function dial($pno)
+    {
+        // Implementing dial($pno) method.
+    }
+
+    public function hangup()
+    {
+        //  Implementing hangup() method.
+    }
+
 }
